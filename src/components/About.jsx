f256be66e7deb5fad008e0e1670b2956972b94ce
Umar Farooq <umarfarooq53067@gmail.com>
2025-05-29 17:41:42 +0500

    picture added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
         <h2 className='text-4xl font-bold text-center mb-12'>About Me</h2>
         <div className="flex flex-col md:flex-row items-center gap-10">
           <img
-            src="/umarfarooq.jpg"
+            src="umar.jpg"
             alt="Umar Farooq"
             loading="lazy"
             className="w-72 h-80 object-cover rounded-xl shadow-lg transition-transform duration-300 hover:scale-105"
